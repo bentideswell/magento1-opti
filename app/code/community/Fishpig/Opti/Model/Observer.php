dacef2f1eab455efa53d30dc515702651ec37771
Ben Tideswell <ben@fishpig.co.uk>
2017-07-28 10:41:30 +0100

    2.2.1.3

--- app/code/community/Fishpig/Opti/Model/Observer.php
+++ app/code/community/Fishpig/Opti/Model/Observer.php
@@ -382,7 +382,12 @@ class Fishpig_Opti_Model_Observer extends Varien_Object
       );
 
       foreach($files as $file) {
-        include($file);
+		if (is_file($file)) {
+        	include($file);
+		}
+		else {
+			Mage::log($file . ' not found in Opti.', true, 'opti.log', true);
+		}
       }
     }
 	}
