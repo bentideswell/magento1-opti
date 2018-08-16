ed5306b377cf667ae6a4eed344d2103ffaf4a066
Ben Tideswell <ben@fishpig.co.uk>
2018-08-16 13:57:08 +0100

    2.2.2.12

--- app/code/community/Fishpig/Opti/Block/System/Config/Form/Field/License.php
+++ app/code/community/Fishpig/Opti/Block/System/Config/Form/Field/License.php
@@ -1,9 +1,11 @@
 <?php
-/**
+/*
  * @category Fishpig
  * @package Fishpig_Wordpress
  * @license http://fishpig.co.uk/license.txt
  * @author Ben Tideswell <help@fishpig.co.uk>
+ * @SkipInject
+ * @Obfuscate
  */
 	
 class Fishpig_Opti_Block_System_Config_Form_Field_License extends Mage_Adminhtml_Block_System_Config_Form_Field
@@ -14,10 +16,8 @@ class Fishpig_Opti_Block_System_Config_Form_Field_License extends Mage_Adminhtml
 	 * @param Varien_Data_Form_Element_Abstract $element
 	 * @return string
 	 */
-    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
-    {
-		return Mage::helper(
-			str_replace('wordpress_license_', '', $element->getHtmlId()) . '/license'
-		)->getImageHtml('sconfig.png');
-    }
+  protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+  {
+		return Mage::helper('opti/license')->getImageHtml('sconfig.png');
+  }
 }
